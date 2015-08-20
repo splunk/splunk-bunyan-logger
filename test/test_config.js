cb212bb8ba7ac4406c612ff2cd4a3253e7704523
Shakeel Mohamed <smohamed@splunk.com>
2015-08-19 19:51:56 -0700

    Add a test for non-default middleware

--- test/test_config.js
+++ test/test_config.js
@@ -96,4 +96,31 @@ describe("createStream", function() {
         assert.strictEqual("info", splunkBunyanStream.stream.config.level);
         assert.strictEqual(8088, splunkBunyanStream.stream.config.port);
     });
+    it("should create logger with non-default middleware", function() {
+        var config = {
+            token: "a-token-goes-here-usually",
+            middleware: function(config, data, callback) {
+                callback(null, null, null);
+            }
+        };
+        var splunkBunyanStream = splunkBunyan.createStream(config);
+
+        assert.ok(splunkBunyanStream);
+        assert.strictEqual("info", splunkBunyanStream.level);
+        assert.strictEqual("raw", splunkBunyanStream.type);
+        assert.ok(splunkBunyanStream.hasOwnProperty("stream"));
+        assert.ok(splunkBunyanStream.stream.hasOwnProperty("config"));
+        assert.ok(splunkBunyanStream.stream.hasOwnProperty("error"));
+        assert.ok(splunkBunyanStream.stream.hasOwnProperty("send"));
+        assert.ok(splunkBunyanStream.stream.hasOwnProperty("end"));
+        assert.strictEqual(config.token, splunkBunyanStream.stream.config.token);
+        assert.strictEqual("splunk-bunyan-logger/0.8.0", splunkBunyanStream.stream.config.name);
+        assert.strictEqual("localhost", splunkBunyanStream.stream.config.host);
+        assert.strictEqual("/services/collector/event/1.0", splunkBunyanStream.stream.config.path);
+        assert.strictEqual(config.middleware, splunkBunyanStream.stream.config.middleware);
+        assert.strictEqual(true, splunkBunyanStream.stream.config.useHTTPS);
+        assert.strictEqual(false, splunkBunyanStream.stream.config.strictSSL);
+        assert.strictEqual("info", splunkBunyanStream.stream.config.level);
+        assert.strictEqual(8088, splunkBunyanStream.stream.config.port);
+    });
 });
